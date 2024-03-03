cc4cb7ccf0adda39badbcc8510a6962c08a662ec
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 14:29:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = new Map();
+	for(let ch of str){
+		charCount.set(ch,(charCount.get(ch) || 0) + 1);
+	}
+	for (let ch of str) {
+		if(charCount.get(ch) === 1){
+			return ch;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
